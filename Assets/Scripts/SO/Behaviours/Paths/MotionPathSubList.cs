05268f4bf1542465bcbaaac39ab9d20abb6ac848
Bruno Ortiz <brunoorla@gmail.com>
2016-12-11 00:24:42 +0100

    NEW: Added the actual paths for each grid size (MotionBehaviour) + public fields isValid/allPathsValid to validate paths at edit time

--- Assets/Scripts/SO/Behaviours/Paths/MotionPathSubList.cs
+++ Assets/Scripts/SO/Behaviours/Paths/MotionPathSubList.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class MotionPathSubList : ScriptableObject {
 
 	public int gridSize = 3;
-
+	public bool allPathsValid;
 	public Path[] paths;
 
 
@@ -25,17 +25,42 @@ public class MotionPathSubList : ScriptableObject {
 		if (gridSize <= 3)
 			gridSize = 3;
 
-		foreach (Path path in paths) {
-			if (path.motions.Length != gridSize * gridSize) {
-				path.motions = new int[gridSize * gridSize];
+		allPathsValid = true;
+		for (int i = 0; i < paths.Length; i++) {
+			if (paths[i].motions.Length != gridSize * gridSize) {
+				paths[i].motions = new int[gridSize * gridSize];
 			}
-		}
 
+			bool valid = true;
+			bool[] checkArray = new bool[paths[i].motions.Length];
+			for (int j = 0; j < checkArray.Length; j++) {
+				checkArray [j] = false;
+			}
+
+			for (int j = 0; j < paths[i].motions.Length; j++) {
+				if (paths [i].motions [j] < 0 || paths [i].motions [j] > checkArray.Length - 1) {
+					valid = false;
+					allPathsValid = false;
+					break;
+				}
+				if (checkArray [paths[i].motions [j]] == false) {
+					checkArray [paths[i].motions [j]] = true;
+				} else {
+					valid = false;
+					allPathsValid = false;
+					break;
+				}
+			}
+			paths[i].isValid = valid;
+		}
 	}
 
 }
 
 [System.Serializable]
 public class Path {
+
+	public bool isValid;
 	public int[] motions;
+
 }
