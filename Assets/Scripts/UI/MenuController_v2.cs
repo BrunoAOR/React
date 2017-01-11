9fe5486733a22c389036b3fab8468ac2d8d1e288
Bruno Ortiz <brunoorla@gmail.com>
2017-01-11 19:16:10 +0100

    CHANGED: Tentative new menu design and reordered some project folders to separate old UI from new UI.

--- Assets/Scripts/UI/MenuController_v2.cs
+++ Assets/Scripts/UI/MenuController_v2.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class MenuController_v2 : MenuControllerBase {
 
+	[Header ("Welcome Screen")]
+	public MenuIntroController introController;
+	public Transform gameModePanels;
 
 	public override void SetActive (bool _) {
 		gameObject.SetActive (_);
 	}
 
+	void Awake () {
+		gameModePanels.gameObject.SetActive (false);
+	}
+
+	IEnumerator Start () {
+		yield return (introController.Intro ());
+		StartCoroutine (PopMenu ());
+	}
+
 	public override IEnumerator PopMenu () {
 
+		yield return new WaitForSeconds (1);
+		gameModePanels.gameObject.SetActive (true);
 		yield break;
 	}
 }
