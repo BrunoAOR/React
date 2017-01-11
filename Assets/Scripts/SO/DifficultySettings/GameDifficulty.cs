315c4349cafff25d59e4225602d8a0560d450f62
Bruno Ortiz <brunoorla@gmail.com>
2017-01-11 15:05:17 +0100

    NEW: Created the GameDifficulty ScriptableObject Script and created assets for the Easy, Medium and Hard difficulties.

---
+++ Assets/Scripts/SO/DifficultySettings/GameDifficulty.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu (menuName = "Game Difficulty", fileName = "New Game Difficulty")]
+public class GameDifficulty : ScriptableObject {
+
+	public string difficulty;
+	public GridSize gridSize;
+	public ButtonsBehaviour[] buttonsBehaviours;
+}
