603dbcb66fbe8e8dab6499a292aaec6d794d6cb7
Bruno Ortiz <brunoorla@gmail.com>
2017-01-15 00:13:20 +0100

    CHANGED: Adjusted difficulty buttons lock shaking parameters and enabled general lock shaking. Fixed Lock sprite to aligned right side of the loop.

---
+++ Assets/Scripts/UI/New UI/MenuGeneralLock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuGeneralLock : MonoBehaviour {
+	private UIShaker _uiShaker;
+
+	void Awake () {
+		_uiShaker = GetComponent<UIShaker> ();
+	}
+
+	public void OnClick () {
+		Managers.Audio.PlaySFX (SFX.IconClicked_Locked);
+		if (_uiShaker != null) {
+			_uiShaker.StartShakeRotate ();
+		}
+	}
+}
