a733f2a82d995f3528b97594aa7dbebbc86c302b
Bruno Ortiz <brunoorla@gmail.com>
2017-03-03 14:42:50 +0100

    NEW: Added a ClickDetection system (placed in the UICanvas > TESTSection) that allows clicks to be shown. This is useful if a video of the screen is made for demonstration

---
+++ Assets/Scripts/Z_Helpers/ClickDetection/ClickDetection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickDetection : MonoBehaviour {
+
+	public ClickSign clickSignPrefab;
+	public Transform clickSignPoolTransform;
+
+	private ComponentPool<ClickSign> clickSignPool;
+
+	private void Awake () {
+		clickSignPool = new ComponentPool<ClickSign> (clickSignPrefab, clickSignPoolTransform);
+		clickSignPrefab.gameObject.SetActive (false);
+
+	}
+
+	private void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			ShowClick (Input.mousePosition);
+		}
+	}
+
+	private void ShowClick (Vector2 screenPosition) {
+		ClickSign sign = clickSignPool.Spawn ();
+		sign.SetParent (transform, false);
+		sign.SetPosition (screenPosition);
+	}
+
+}
---
+++ Assets/Scripts/Z_Helpers/ClickDetection/ClickSign.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSign : MonoBehaviour, IPoolable<ClickSign> {
+
+	public float lifeTime;
+	public Vector2 initialSize;
+	public Vector2 finalSize;
+
+	public UnspawnDelegate<ClickSign> Unspawn { get; set;}
+
+	private RectTransform _rectTransform;
+	private float timeStart;
+
+	private void Awake () {
+		_rectTransform = GetComponent<RectTransform> ();
+	}
+
+	private void OnEnable () {
+		timeStart = Time.time;
+	}
+
+	private void Update () {
+
+		if (Time.time - timeStart > lifeTime) {
+			_rectTransform.sizeDelta = initialSize;
+			Unspawn (this);
+			return;
+		}
+
+		float u = (Time.time - timeStart) / lifeTime;
+		Vector2 currentSize = Vector2.Lerp (initialSize, finalSize, u);
+
+		_rectTransform.sizeDelta = currentSize;
+	}
+
+	public void SetPosition (Vector2 position) {
+		_rectTransform.position = position;
+	}
+
+	public void SetParent (Transform parent, bool worldPositionStays) {
+		_rectTransform.SetParent (parent, worldPositionStays);
+	}
+
+
+
+
+
+}
